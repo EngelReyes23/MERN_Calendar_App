5b612a40e63563402779b0d055b5dfd0b915c018
EngeReyes23 <engelreyes23@gmail.com>
2022-04-09 23:20:42 -0600

    loading spinner agregado

---
+++ src/components/ui/Loading.jsx
@@ -0,0 +1,11 @@
+import './loading.css';
+
+export const Loading = () => (
+  <div className='loadingContainer'>
+    <div class='loading'>
+      <div class='arc'></div>
+      <div class='arc'></div>
+      <div class='arc'></div>
+    </div>
+  </div>
+);
